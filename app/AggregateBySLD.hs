490c8276189d38d9f515bd4cae08e9586a4c138f
adamConnerSax <adam_conner_sax@yahoo.com>
2019-12-13 15:33:21 -0500

    GA works.  TX is annoying.

--- app/AggregateBySLD.hs
+++ app/AggregateBySLD.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveFunctor      #-}
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections      #-}
 {-# LANGUAGE TypeApplications   #-}
 module Main where
@@ -12,9 +13,12 @@ import qualified Data.List                     as L
 import qualified Data.Map                      as M
 import           Data.Maybe                     ( isJust
                                                 , fromJust
+                                                , fromMaybe
                                                 )
+import           Data.Ord                       ( Ordering )
 import qualified Data.Set                      as S
 import qualified Data.Text                     as T
+import qualified Data.Text.IO                  as T
 import qualified Data.Char                     as TC
 import qualified Data.ByteString.Lazy          as BL
 import qualified Data.Vector                   as V
@@ -54,76 +58,192 @@ data PrecinctVote =
                , pVote :: CandidateVote
                } deriving (Generic, Show)
 
+type PKey = (T.Text, T.Text) -- (county, precinct)
+
+pKey :: PrecinctVote -> PKey
+pKey pv = (county pv, precinct pv)
 
 instance C.FromNamedRecord PrecinctVote where
   parseNamedRecord m = PrecinctVote
                        <$> m .: "county"
                        <*> m .: "precinct"
-                       <*> (fmap (readMaybe @Int) $ m .: "district")
+                       <*> (fmap (readMaybe @Int . L.filter TC.isDigit) $ m .: "district")
                        <*> (CandidateVote
                             <$> m .: "office"
                             <*> m .: "party"
                             <*> m .: "candidate"
-                            <*> m .: "votes"
+                            <*> (fmap (fromMaybe 0 . readMaybe @Int) $ m .: "votes")
                            )
 
---instance C.DefaultOrdered PrecinctVote
---    "openelections-data-ga/2018/20181106__ga__general__fulton__precinct.csv"
+fixOffice :: PrecinctVote -> PrecinctVote
+fixOffice pv@(PrecinctVote c p dM (CandidateVote o n pa v)) = case dM of
+  Nothing -> pv
+  Just d  -> PrecinctVote
+    c
+    p
+    dM
+    (CandidateVote (o <> ": district " <> (T.pack $ show d)) n pa v)
+
+data StateConfig = StateConfig { getFilePaths :: IO ([FilePath], [FilePath]) }
+
+gaConfig = StateConfig
+  (do
+    let dir = "openelections-data-ga/2018/"
+        addDir f = dir ++ f
+    allFiles <- SD.listDirectory dir
+    let elexFiles = fmap addDir
+          $ L.filter (L.isPrefixOf "20181106__ga__general__") allFiles
+        backupFiles = fmap addDir
+          $ L.filter (L.isPrefixOf "20180522__ga__primary__") allFiles
+    return (elexFiles, backupFiles)
+  )
+
+
+txConfig = StateConfig
+  (do
+    let dir = "openelections-data-tx/2018/counties/"
+        addDir f = dir ++ f
+    allFiles <- SD.listDirectory dir
+    let elexFiles = fmap addDir
+          $ L.filter (L.isPrefixOf "20181106__tx__general__") allFiles
+        backupFiles = fmap addDir
+          $ L.filter (L.isPrefixOf "20180306__tx__primary__") allFiles
+    return (elexFiles, backupFiles)
+  )
+
 main :: IO ()
 main = do
-  let precinctDir    = "openelections-data-ga/2018/"
-      electionPrefix = "20181106__ga__general__"
-  files <- SD.listDirectory precinctDir
-  let filesToProcess = L.filter (electionPrefix `L.isPrefixOf`) files
-  pVotesAll <-
-    mconcat
-      <$> (forM filesToProcess $ \fp -> do
-            putStrLn $ "parsing \"" ++ fp ++ "\""
-            precinctCSV <- BL.readFile (precinctDir ++ fp)
-            case C.decodeByName @PrecinctVote precinctCSV of
-              Left err -> E.throw
-                $ MiscException ("CSV Parse Failure: " <> (T.pack $ show err))
-              Right (_, v) -> return v
-          )
-  putStrLn $ "parsing SLDs..."
-  slds <- case (sequence $ fmap parseSLDFromPV pVotesAll) of
-    Left err ->
-      E.throw $ MiscException ("SLD Parse Failure: " <> (T.pack $ show err))
+  let stateConfig = txConfig
+  (elexFiles, backupPrecFiles) <- getFilePaths stateConfig
+  parsedElection'              <- parseFiles elexFiles
+  let parsedElection = fmap (\(a, b) -> (a, fixOffice b)) parsedElection'
+  parsedBackup <- parseFiles backupPrecFiles
+  putStrLn $ "constructing backup dictionary of precinct -> SLD mappings"
+  backupSLDsBP <- case FL.foldM sldsByPrecinctFM parsedBackup of
+    Left err -> E.throw $ MiscException
+      (  "Backup precinct -> SLDs dictionary build failure: "
+      <> (T.pack $ show err)
+      )
     Right x -> return x
-  putStrLn $ "Doing pass 1: sldsByPrecinct and vboPrecinct"
-  let withSLDs = V.zip slds pVotesAll
-      allFM =
+  putStrLn
+    $ "Doing pass 1: sldsByPrecinct, vboPrecinct and primary precinct -> SLDs dictionary"
+  let allFM =
         (,,)
           <$> (FL.generalize precinctsBySLDF)
           <*> (FL.generalize tvboFromPrecinctsF)
           <*> sldsByPrecinctFM
   (precinctsBySLDs, vboPrecinct, sldsByPrecinct) <-
-    case FL.foldM allFM withSLDs of
+    case FL.foldM allFM parsedElection of
       Left err ->
         E.throw $ MiscException ("Pass 1 Failure: " <> (T.pack $ show err))
       Right x -> return x
   putStrLn $ "pass 1 complete.\npass 2: votesBySLD and vboHouse, vboSenate"
-  votesBySLD <- case FL.foldM (votesBySLDFM sldsByPrecinct) pVotesAll of
-    Left err -> E.throw
-      $ MiscException ("Vote re-allocation error: " <> (T.pack $ show err))
-    Right x -> return x
+  putStrLn $ "Backfilling primary dictionary:"
+  updatedSLDsByPrecinct <- backfillDictionary sldsByPrecinct backupSLDsBP
+  votesBySLD            <-
+    case
+      FL.foldM (votesBySLDFM updatedSLDsByPrecinct) (fmap snd parsedElection)
+    of
+      Left err -> E.throw
+        $ MiscException ("Vote re-allocation error: " <> (T.pack $ show err))
+      Right x -> return x
   let votesF =
         (,)
           <$> FL.prefilter (isHouse . fst) tvboFromSLDF
           <*> FL.prefilter (isSenate . fst) tvboFromSLDF
       (vboHouse, vboSenate) = FL.fold votesF votesBySLD
-  case (vboPrecinct == vboHouse) && (vboHouse == vboSenate) of
-    True  -> putStrLn $ "Vote totals match."
-    False -> do
-      putStrLn
-        $  "Vote totals mismatched!"
-        ++ "by precinct:\n"
-        ++ show vboPrecinct
-        ++ "by house districts:\n"
-        ++ show vboHouse
-        ++ "by senate districts:\n"
-        ++ show vboSenate
-      return ()
+      voteComp x y = if x == y then Nothing else Just (x - y)
+      pVsHouse      = M.differenceWith voteComp vboPrecinct vboHouse
+      pVsSenate     = M.differenceWith voteComp vboPrecinct vboSenate
+      houseVsSenate = M.differenceWith voteComp vboHouse vboSenate
+  putStrLn $ "Precinct vs House:\n" ++ show pVsHouse
+  putStrLn $ "Precinct vs Senate:\n" ++ show pVsSenate
+  putStrLn $ "House vs Senate:\n" ++ show houseVsSenate
+  let output = vbSLDHeader <> "\n" <> T.intercalate
+        "\n"
+        (fmap vbSLDtoCSV $ L.sortBy outputCompare $ L.filter outputFilter
+                                                             votesBySLD
+        )
+  T.writeFile "VotesByStateLegistiveDistrict.csv" output
+  return ()
+
+outputFilter :: (SLD (), CandidateVote) -> Bool
+outputFilter (_, (CandidateVote o _ _ _)) =
+  let hasWord word text = not . T.null . snd $ word `T.breakOn` text
+      isGov    = hasWord "Governor" o
+      isSenate = hasWord "Senator" o && not (hasWord "State" o)
+      isHouse  = hasWord "Representative" o && not (hasWord "State" o)
+  in  isGov || isSenate || isHouse
+
+outputCompare :: (SLD (), CandidateVote) -> (SLD (), CandidateVote) -> Ordering
+outputCompare (sldA, (CandidateVote oA nA _ _)) (sldB, (CandidateVote oB nB _ _))
+  = compare oA oB <> compare sldA sldB <> compare nA nB
+
+
+backfillDictionary
+  :: SLDsByPrecinct Double
+  -> SLDsByPrecinct Double
+  -> IO (SLDsByPrecinct Double)
+backfillDictionary electionD backupD = do
+  let
+    missingHouse  = L.null . L.filter isHouse
+    missingSenate = L.null . L.filter isSenate
+    multiHouse    = (> 1) . L.length . L.filter isHouse
+    multiSenate   = (> 1) . L.length . L.filter isSenate
+    addFromBackup isType (p, slds) =
+      case fmap (L.filter isType) (M.lookup p backupD) of
+        Nothing ->
+          E.throw
+            $  MiscException
+            $  "Failed to find precinct=\""
+            <> (T.pack $ show p)
+            <> "\" in backup dictionary."
+        Just [] ->
+          E.throw
+            $  MiscException
+            $  "Backup entry for precinct=\""
+            <> (T.pack $ show p)
+            <> "\" is also empty."
+        Just newSLDs -> return (p, slds ++ newSLDs)
+
+    findF =
+      (,,,)
+        <$> FL.prefilter (missingHouse . snd) FL.list
+        <*> FL.prefilter (missingSenate . snd) FL.list
+        <*> FL.prefilter (multiHouse . snd) FL.list
+        <*> FL.prefilter (multiSenate . snd) FL.list
+    (needBackupH, needBackupS, multiH, multiS) =
+      FL.fold findF (M.toList electionD)
+  withBackupH <- traverse (addFromBackup isHouse) needBackupH
+  withBackupS <- traverse (addFromBackup isSenate) needBackupS
+  putStrLn $ "House from backup: " ++ show withBackupH
+  putStrLn $ "Senate from backup: " ++ show withBackupS
+  putStrLn $ "Multi-House: " ++ show multiH
+  putStrLn $ "Multi-Senate: " ++ show multiS
+  let fromBackupM =
+        M.unionWith (<>) (M.fromList withBackupH) (M.fromList withBackupS) -- in case same precinct is on both lists
+      newSLDsByPrecinct = M.union fromBackupM electionD -- prefer updated  
+  return newSLDsByPrecinct
+
+
+
+parseFiles :: [FilePath] -> IO (V.Vector (Maybe (SLD Int), PrecinctVote))
+parseFiles fps = do
+  pVotes <-
+    mconcat
+      <$> (forM fps $ \fp -> do
+            putStrLn $ "loading/parsing \"" ++ fp ++ "\""
+            precinctCSV <- BL.readFile fp
+            case C.decodeByName @PrecinctVote precinctCSV of
+              Left err -> E.throw
+                $ MiscException ("CSV Parse Failure: " <> (T.pack $ show err))
+              Right (_, v) -> return v
+          )
+  slds <- case (sequence $ fmap parseSLDFromPV pVotes) of
+    Left err ->
+      E.throw $ MiscException ("SLD Parse Failure: " <> (T.pack $ show err))
+    Right x -> return x
+  return $ V.zip slds pVotes
 
 data Candidate = Candidate { cName :: T.Text, cParty :: T.Text }
 
@@ -141,8 +261,8 @@ isHouse _           = False
 isSenate (Senate _ _) = True
 isSenate _            = False
 
-type PrecinctsBySLD = M.Map (SLD ()) (S.Set T.Text)
-type SLDsByPrecinct a = M.Map T.Text [SLD a]
+type PrecinctsBySLD = M.Map (SLD ()) (S.Set PKey)
+type SLDsByPrecinct a = M.Map PKey [SLD a]
 
 data ProcessedPrecincts = ProcessedPrecincts { precinctsBySLD :: PrecinctsBySLD
                                              , votesByPrecinct :: VotesByPrecinct
@@ -165,6 +285,17 @@ votesBySLDFM sldsByP = MR.mapReduceFoldM
     (\(sld, co, cn, cp) v -> (sld, CandidateVote co cn cp v))
   )
 
+vbSLDHeader :: T.Text =
+  "State Legislative District,Office,Candidate,Party,Votes"
+
+vbSLDtoCSV :: (SLD (), CandidateVote) -> T.Text
+vbSLDtoCSV (sld, (CandidateVote o n p v)) =
+  sldName sld <> "," <> o <> "," <> n <> "," <> p <> "," <> (T.pack $ show v)
+
+sldName :: SLD a -> T.Text
+sldName (House  n _) = "House " <> (T.pack $ show n)
+sldName (Senate n _) = "Senate " <> (T.pack $ show n)
+
 sldLabel :: SLD a -> SLD ()
 sldLabel = fmap (const ())
 
@@ -172,11 +303,17 @@ sldData :: SLD a -> a
 sldData (House  _ x) = x
 sldData (Senate _ x) = x
 
+sumDistrictData :: Num a => [SLD a] -> [SLD a]
+sumDistrictData slds =
+  fmap (\(l, d) -> fmap (const d) l) $ M.toList $ M.fromListWith (+) $ zip
+    (fmap sldLabel slds)
+    (fmap sldData slds)
+
 precinctVotesToSLDVotes
   :: SLDsByPrecinct Double
   -> PrecinctVote
   -> Either T.Text [(SLD (), CandidateVote)]
-precinctVotesToSLDVotes sldsByP pv = case M.lookup (precinct pv) sldsByP of
+precinctVotesToSLDVotes sldsByP pv = case M.lookup (pKey pv) sldsByP of
   Nothing   -> Left $ "No SLDs found for precinct=\"" <> (precinct pv) <> "\"."
   Just slds -> do
     let withSLD pv sld = (sldLabel sld, scaleVotes (sldData sld) $ pVote pv)
@@ -185,7 +322,7 @@ precinctVotesToSLDVotes sldsByP pv = case M.lookup (precinct pv) sldsByP of
 precinctsBySLDF :: FL.Fold (Maybe (SLD Int), PrecinctVote) PrecinctsBySLD
 precinctsBySLDF = fmap (fmap S.fromList . M.fromList) $ MR.mapReduceFold
   (MR.filterUnpack (isJust . fst))
-  (MR.assign (fromJust . fst) (precinct . snd))
+  (MR.assign (fromJust . fst) (pKey . snd))
   (MR.foldAndLabel FL.list (\sld ps -> (sldLabel sld, ps)))
 
 sldsByPrecinctFM
@@ -195,35 +332,29 @@ sldsByPrecinctFM
        (SLDsByPrecinct Double)
 sldsByPrecinctFM = fmap M.fromList $ MR.mapReduceFoldM
   (MR.generalizeUnpack $ MR.filterUnpack (isJust . fst))
-  (MR.generalizeAssign $ MR.assign (precinct . snd) (fromJust . fst))
+  (MR.generalizeAssign $ MR.assign (pKey . snd) (fromJust . fst))
   (MR.ReduceFoldM f)
 
-f :: T.Text -> FL.FoldM (Either T.Text) (SLD Int) (T.Text, [SLD Double])
+f :: PKey -> FL.FoldM (Either T.Text) (SLD Int) (PKey, [SLD Double])
 f p = fmap (p, ) $ MR.postMapM (sldsVotesToWeights p) (FL.generalize FL.list)
 
-sldsVotesToWeights :: T.Text -> [SLD Int] -> Either T.Text [SLD Double]
+sldsVotesToWeights :: PKey -> [SLD Int] -> Either T.Text [SLD Double]
 sldsVotesToWeights p slds = do
-  let hSLDs = filter isHouse slds
-      sSLDs = filter isSenate slds
+  let wgtError seatType p =
+        (T.pack $ show p)
+          <> " has >1 "
+          <> seatType
+          <> " districts but weights sum to 0: "
       toWeight total n = (realToFrac n) / (realToFrac total)
-  when (L.null hSLDs) $ Left $ p <> " has no state house districts."
-  when (L.null sSLDs) $ Left $ p <> " has no state senate districts."
-  let hTotal = FL.fold (FL.premap sldData FL.sum) hSLDs
-  when (hTotal == 0)
-    $  Left
-    $  p
-    <> " has house votes summing to 0 ("
-    <> (T.pack $ show slds)
-    <> ")"
-  let whSLDs = fmap (fmap (toWeight hTotal)) hSLDs
-  let sTotal = FL.fold (FL.premap sldData FL.sum) sSLDs
-  when (sTotal == 0)
-    $  Left
-    $  p
-    <> " has senate votes summing to 0 ("
-    <> (T.pack $ show slds)
-    <> ")"
-  let wsSLDs = fmap (fmap (toWeight sTotal)) sSLDs
+      weightSLDs p seatType slds = case sumDistrictData slds of
+        []  -> Right []
+        [x] -> Right [fmap (const 1) x]
+        xs  -> do
+          let t = FL.fold (FL.premap sldData FL.sum) xs
+          when (t == 0) $ Left $ wgtError seatType p <> (T.pack $ show xs)
+          return $ fmap (fmap $ toWeight t) xs
+  whSLDs <- weightSLDs p "House" $ filter isHouse slds
+  wsSLDs <- weightSLDs p "Senate" $ filter isSenate slds
   return $ whSLDs ++ wsSLDs
 
 
